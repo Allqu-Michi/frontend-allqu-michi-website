02dd9a510f34d1cf072f1e4f44a586180d4efec8
Gherard Chipana <2014100007@ucss.pe>
2022-08-06 16:41:46 -0500

    agregando logica para cambio de colores de boton seleccionado de filtro (#2)
    Co-authored-by: boening800 <boening800@gmail.com>

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -12,36 +12,26 @@ export class HeaderComponent implements OnInit {
   @ViewChild('AllquMichiRef') AllquMichiRef: ElementRef = {} as ElementRef;
 
   constructor(
-    private _renderer:Renderer2,
-    private _elementRef: ElementRef 
+    private _renderer:Renderer2
   ) { }
 
   ngOnInit(): void {
-    // console.log(document.getElementsByClassName('btn').length);
-    document.getElementsByClassName('btn')[0].classList.add('select');
   }
 
-  selectOptionDog(event:any){
-    // for (let i = 0; i < document.getElementsByClassName('btn').length; i++) {
-    //   if (document.getElementsByClassName('btn')[i].classList.contains('select')) {
-    //     document.getElementsByClassName('btn')[i].classList.remove('select');
-    //   }else{
-    //     document.getElementsByClassName('btn')[i].classList.add('select');
-    //   }
-    // }
-    console.log(this.AllquRef.nativeElement)
-    // console.log(this._elementRef.nativeElement)
-    // this._renderer.addClass(this._elementRef.nativeElement, 'select');
-    // console.log( this.AllquRef.nativeElement);
-    // this.AllquRef.nativeElement.classList.addClass('select')
-    this._renderer.addClass(this.AllquRef.nativeElement,'select')
+  selectOptionDog(){
+    this._renderer.addClass(this.AllquRef.nativeElement,'select');
+    this._renderer.removeClass(this.MichiRef.nativeElement,'select');
+    this._renderer.removeClass(this.AllquMichiRef.nativeElement,'select');
   }
-  selectOptionCat(event:any){
-    // this.MichiRef.nativeElement.classList.addClass('select')
-    this._renderer.addClass(this.AllquRef.nativeElement,'select')
+  selectOptionCat(){
+    this._renderer.removeClass(this.AllquRef.nativeElement,'select');
+    this._renderer.addClass(this.MichiRef.nativeElement,'select');
+    this._renderer.removeClass(this.AllquMichiRef.nativeElement,'select');
   }
-  selectOptionAll(event:any){
-
+  selectOptionAll(){
+    this._renderer.removeClass(this.AllquRef.nativeElement,'select');
+    this._renderer.removeClass(this.MichiRef.nativeElement,'select');
+    this._renderer.addClass(this.AllquMichiRef.nativeElement,'select');
   }
 
 }
