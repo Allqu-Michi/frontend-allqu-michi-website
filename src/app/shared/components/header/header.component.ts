ab638fafb86aa357f3abd10c0c7322cf4ad0f1df
Gherard Chipana <2014100007@ucss.pe>
2022-08-10 14:46:39 -0500

    Develop (#7)
    * agregando cambio de prueba en header
    
    * update develop
    
    * conectando servicios web
    
    Co-authored-by: Nogheric <nogheric@gmail.com>
    Co-authored-by: boening800 <boening800@gmail.com>

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, Renderer2, ViewChild } from '@angular/core';
+import { ServicePetsService } from 'src/app/modules/home/pages/pets/services/service-pets.service';
 
 @Component({
   selector: 'app-header',
@@ -11,27 +12,51 @@ export class HeaderComponent implements OnInit {
   @ViewChild('MichiRef') MichiRef: ElementRef = {} as ElementRef;
   @ViewChild('AllquMichiRef') AllquMichiRef: ElementRef = {} as ElementRef;
 
+  @Output() petsO = new EventEmitter<any>();
+  @Output() spinnerO = new EventEmitter<any>();
+
   constructor(
-    private _renderer:Renderer2
+    private _renderer:Renderer2,
+    private _PetsS:ServicePetsService
   ) { }
 
   ngOnInit(): void {
+   this.selectOptionAll();
   }
 
   selectOptionDog(){
+    this.spinnerO.emit(true);
+    this.petsO.emit([]);
+    this._PetsS.getPetById(1).subscribe(data=>{
+      this.petsO.emit(data);
+      this.spinnerO.emit(false);
+    })
+
     this._renderer.addClass(this.AllquRef.nativeElement,'select');
     this._renderer.removeClass(this.MichiRef.nativeElement,'select');
     this._renderer.removeClass(this.AllquMichiRef.nativeElement,'select');
   }
   selectOptionCat(){
+    this.spinnerO.emit(true);
+    this.petsO.emit([]);
+    this._PetsS.getPetById(2).subscribe(data=>{
+      this.petsO.emit(data);
+      this.spinnerO.emit(false);
+    })
     this._renderer.removeClass(this.AllquRef.nativeElement,'select');
     this._renderer.addClass(this.MichiRef.nativeElement,'select');
     this._renderer.removeClass(this.AllquMichiRef.nativeElement,'select');
   }
   selectOptionAll(){
+    this.spinnerO.emit(true);
+    this.petsO.emit([]);
+    this._PetsS.getPets().subscribe(data=>{
+      this.petsO.emit(data);
+      this.spinnerO.emit(false);
+    })
     this._renderer.removeClass(this.AllquRef.nativeElement,'select');
     this._renderer.removeClass(this.MichiRef.nativeElement,'select');
-    this._renderer.addClass(this.AllquMichiRef.nativeElement,'select');
+    this._renderer.addClass(this.AllquMichiRef.nativeElement,'select');    
   }
 
 }
