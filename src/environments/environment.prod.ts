7c17a56a5f31aa5cbc60b75de093ff41d64f6845
boening800 <boening800@gmail.com>
2022-08-29 19:42:06 -0500

    actualizando variables de enviroment prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl:"https://dyrblnofysgjzhzcbbip.supabase.co/rest/v1/"
+  baseUrl:"https://ecrhlxrwpezoizsstjdx.supabase.co",
+  supabaseKey:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVjcmhseHJ3cGV6b2l6c3N0amR4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAxOTUzOTMsImV4cCI6MTk3NTc3MTM5M30.RII3FVYL99dyrEcZHujgl42vrYX6ok2AHcb3EFGO480"
 };
